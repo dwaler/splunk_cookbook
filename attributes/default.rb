ff0ac996c9f54f390eac30eeec51960475a06788
M. G. (Michael) de Bruin <mdebruin@schubergphilis.com>
2013-11-27 11:00:10 +0100

    Leave the default port on default please

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,7 @@ default['splunk']['server_home']               = "/opt/splunk"
 default['splunk']['db_directory']              = "/volr/splunk"
 
 #web config
-default['splunk']['web_server_port']           = "80" # Change to 443/other if you're doing ssl
+default['splunk']['web_server_port']           = "8000" # Change to 443/other if you're doing ssl
 default['splunk']['root_endpoint']             = "/" # Web Endpoint
 default['splunk']['browser_timeout']           = "0"
 default['splunk']['minify_js']                 = "true"
